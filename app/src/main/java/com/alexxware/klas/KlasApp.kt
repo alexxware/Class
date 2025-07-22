b03821205a8bfa2fffd191b046dbfb806d1bc120
alexxware <manuelmorm1@gmail.com>
2025-07-22 09:55:13 -0600

    primer commit

---
+++ app/src/main/java/com/alexxware/klas/KlasApp.kt
@@ -0,0 +1,8 @@
+package com.alexxware.klas
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class KlasApp: Application() {
+}
---
+++ app/src/main/java/com/alexxware/klas/ui/screens/auth/LoginScreen.kt
@@ -0,0 +1,9 @@
+package com.alexxware.klas.ui.screens.auth
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+
+}
